ffae55c58d30812cb5b98a79b29c6b6fd85093ae
Harsh-Koundal <aharsh33039@gmail.com>
2026-01-07 08:51:21 -0800

    New Project

---
+++ Email-Service/models/User.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:3,
+        select:false,
+    },
+    role:{
+        type:String,
+        enum:["USER","ADMIN"],
+        default:"USER",
+    },
+    refreshToken:{
+        type:String,
+        select:false,
+    },
+},
+{timestamps:true},
+);
+
+userSchema.pre("save", async function () {
+    // Only hash if password is new or modified
+    if(!this.isModified("password")) return;
+
+    const SALT_ROUNDS = 12;
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+});
+
+
+// COMPARE PASSWORD METHOD
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+// REMOVE SENSITIVE FIELDS FROM JSON OUTPUT
+userSchema.methods.toJSON = function () {
+    const user = this.toObject();
+    delete user.password;
+    delete user.refreshToken;
+    return user;
+};
+
+export const User = mongoose.model("User", userSchema);
